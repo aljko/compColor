e566c08bee8b8a5b9827500bccf727e66af1fdfa
aljko <aljkoroz@gmail.com>
2021-06-28 17:13:09 +0200

    Mise en page

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 function randColor(){
-    return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    // Création d'un couleur aléatoire
+    let response = (Math.random()*0xFFFFFF<<0).toString(16);
+    // Création d'une couleur aléatoire
+    if (response.length == 5){
+        response = "0"+response;
+    }
     // Creer une condition pour gerer les >6 chiffres.
+    return '#'+ response;
 }
 
 function compColor(color){
     return '#'+ (0xffffff - ('0x'+(color.substring(1)))).toString(16)
 }
 
+for(let i=0; i<2; i++){
+    let couple =  document.createElement('div');
+    //
+    couple.style.border = "medium solid #000000";
+    //
+    document.body.appendChild(couple);
+    //
 
-for(let i=0; i<1; i++){
     let newCube = document.createElement('div');
     // Création d'un nouvel élément div
     let newCubeColor = randColor();
@@ -18,10 +28,12 @@ for(let i=0; i<1; i++){
     // Inscription de l'héxa de couleur
     newCube.style.backgroundColor = newCubeColor;
     // Application de la nouvelle couleur
-    newCube.style.width = '25%';
-    newCube.style.height = '25%';
+    newCube.style.width = '150px';
+    newCube.style.height = '150px';
     document.body.appendChild(newCube);
     // Ajout au DOM
+    couple.append(newCube);
+    //
 
     let newCubeComp = document.createElement('div');
     // Création d'un nouvel élément div
@@ -31,10 +43,11 @@ for(let i=0; i<1; i++){
     // Inscription de l'héxa de la couleur
     newCubeComp.style.backgroundColor = newCubeCompColor;
     // Application de la nouvelle couleur
-    newCube.style.width = '25%';
-    newCube.style.height = '25%';
+    newCubeComp.style.width = '150px';
+    newCubeComp.style.height = '150px';
+    
     document.body.appendChild(newCubeComp);
     // Ajout au DOM
-
-
+    couple.appendChild(newCubeComp);
+    //
 }
